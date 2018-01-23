60898db68f75eaa329672ae428547605ce94b087
Seohyun Yoon <sarahseohyun@gmail.com>
2018-01-23 12:46:47 +0900

    GRAPH 다시 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
+app_name='dic'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^content$', views.content, name='content'),
-    url(r'^result$', views.result, name='result'), 
-
+    url(r'^result$', views.result, name='result'),
+    url(r'^word$', views.word_graph, name='word_graph'),
 ]
