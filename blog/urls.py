e40105f6283a55f6f8c4a5ad2b58e6489cd49e75
shinah <shinah1234@naver.com>
2018-03-04 17:32:28 +0900

    url수정

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^content$', views.content, name='content'),
     url(r'^result$', views.result, name='result'),
-    
-    #회원관리 관련
     url(r'^sign_up/$', views.signup, name='sign_up'),
     url(r'^sign_in/$', views.signin, name='sign_in'),
     url('^sign_out/$', auth_views.logout, {'next_page' : '/'}),
