04faa5276ea779420454bc6989dd9a6be28ed899
jungjisoo <wltn716@naver.com>
2018-03-17 17:42:43 +0900

    detail, list 삭제

--- blog/urls.py
+++ blog/urls.py
@@ -9,12 +9,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^content$', views.content, name='content'),
     url(r'^result$', views.result, name='result'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
-
-
-    url(r'^admin/', admin.site.urls),
-    url(r'^post_list$', views.post_list, name='post_list'),
     url(r'^sign_up/$', views.signup, name='sign_up'),
     url(r'^sign_in/$', views.signin, name='sign_in'),
     url('^sign_out/$', auth_views.logout, {'next_page' : '/'}),
