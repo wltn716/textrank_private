30b5f326ef380ae66c9672f8c4545877bc2498e0
jungjisoo <wltn716@naver.com>
2018-03-17 17:39:24 +0900

    sign_in, sign_up 프론트

--- blog/forms.py
+++ blog/forms.py
@@ -12,8 +12,17 @@ class UserForm(forms.ModelForm):
   class Meta:
     model = User
     fields = ['username', 'email', 'password']
+    widgets = {
+            'username': forms.TextInput(attrs={'placeholder':'Name'}),
+            'email': forms.EmailInput(attrs={'placeholder':'Email'}),
+            'password' : forms.PasswordInput(attrs={'placeholder':'Password'}),
+        }
 
 class LoginForm(forms.ModelForm):
   class Meta:
     model = User
     fields = ['username', 'password'] # 로그인 시에는 유저이름과 비밀번호만 입력 받는다.
+    widgets = {
+            'username': forms.TextInput(attrs={'placeholder':'Name'}),
+            'password' : forms.PasswordInput(attrs={'placeholder':'Password'}),
+        }
